487a5fa8994d68c8bd17549607e3485dfc9c577e
Harrison <steeleharrison2@gmail.com>
2026-02-02 09:06:20 -0600

    Update longvariables.ino

--- longvariables.ino
+++ longvariables.ino
@@ -1,32 +1,51 @@
-// project 14
-
-long number = 0;
+// project 14 modified complete
+long base = 0;
+long exponent = 0;
+double result = 1;
 long a = 0;
 
 void setup()
 {
-  Serial.begin(9600);
+Serial.begin(9600);
+Serial.println("This program will prompt you to enter a number, which is your base, then enter a second number, which will raise the base to that number.");
+delay(500);
 }
 
 void loop()
 {
-  number = 0;     
-  Serial.flush(); 
-  while (Serial.available() == 0)
-  {
-  }
-    
+  // setting variables
+  base = 0;
+  exponent = 0;
+  result = 0;
+
+  // base 
+  Serial.println("Enter your base now.");
+  while (Serial.available() == 0) {}
   while (Serial.available() > 0)
   {
-    number = number * 10; 
+    base = base * 10;
     a = Serial.read() - '0';
-    number = number + a;
+    base = base + a;
     delay(5);
   }
-  Serial.print("You entered: ");
-  Serial.println(number);
-  Serial.print(number);
-  Serial.print(" multiplied by two is ");
-  number = number * 2;
-  Serial.println(number);
+  // exponent 
+  Serial.println("Now, enter your exponent.");
+  while (Serial.available() == 0) {}
+  while (Serial.available() > 0)
+  {
+    exponent = exponent * 10;
+    a = Serial.read() - '0';
+    exponent = exponent + a;
+    delay(5);
+  }
+  
+result = pow(base, exponent);
+
+  // final result
+  Serial.print(base);
+  Serial.print(" to the power of ");
+  Serial.print(exponent);
+  Serial.print(" is ");
+  Serial.println(result);
+  delay(5);
 }
